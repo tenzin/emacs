588f7eb942a26756f77f2617b42d2bd295300155
Tenzin Dendup <tenzin.dendup@gmail.com>
2022-04-09 13:33:49 +0600

    added notes.org to agenda

--- init.el
+++ init.el
@@ -153,7 +153,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/orgmode/gtd.org" "~/orgmode/from-mobile.org"))
+ '(org-agenda-files
+   '("~/orgmode/gtd.org" "~/orgmode/from-mobile.org" "~/orgmode/notes.org"))
  '(org-agenda-todo-list-sublevels nil)
  '(org-enforce-todo-dependencies t)
  '(package-selected-packages
