e3a10d9b7cddd86632c0fa9841f17c8cde78fc63
tenzin <tenzin.dendup@gmail.com>
2023-08-15 20:33:00 +0800

    emacs config update on 15-8-23

--- init.el
+++ init.el
@@ -93,10 +93,10 @@
 
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
                                (file+headline "~/gtd/inbox.org" "Tasks")
-                               "* TODO %? \n  Entered on: %U\n %i\n %a")
+                               "* TODO %? \nEntered on: %U\n %i\n %a" :empty-lines 1)
                               ("T" "Tickler" entry
                                (file+headline "~/gtd/tickler.org" "Tickler")
-                               "* %i%? \n Entered on: %U\n %i\n %a")))
+                               "* %i%? \nEntered on: %U\n %i\n %a" :empty-lines 1)))
 
 (setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
                            ("~/gtd/someday.org" :level . 1)
@@ -133,15 +133,42 @@
 
 ;; orgmode tags
 (setq org-tag-alist '((:startgroup . nil)
-                      ("OFFICE" . ?o) ("HOME" . ?h)
+                      ("@OFFICE" . ?o) ("@HOME" . ?h) ("@WIFE" . ?w) ("@PHONE" . ?p) ("@COMPUTER" . ?c)
                       (:endgroup . nil)
-                      ("COMPUTER" . ?c) ("PHONE" . ?p) ("READING" . ?r) ("BICYCLE" . ?b)))
+                      ("WRITING" . ?w) ("READING" . ?r) ("BICYCLE" . ?b)))
 
 
 ;; set refile options to show when refiling [C-c C-w]
 (setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
                            ("~/gtd/someday.org" :level . 1)
                            ("~/gtd/tickler.org" :maxlevel . 2)))
+
+
+;; org-agenda custom commands
+(setq org-agenda-custom-commands 
+      '(("h" "At Home" tags-todo "@HOME"
+         ((org-agenda-overriding-header "Things to do at Home")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	("c" "At Computer" tags-todo "@COMPUTER"
+	 ((org-agenda-overriding-header "Things to do on a computer")
+	  (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))))
+
+;; only show the first todo (next to do) from projects using org-agenda-custom-commands
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+		  
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
 
 ;; Make emacs show org-agenda menu on startup - Does not look helpful - hence commented for now
 ;;(add-hook 'after-init-hook 'org-agenda)
